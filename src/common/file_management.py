0320686f0340b3d43fccfbb60d75220f14f4d1d2
bedkowski.patrick <bedkowskisite@gmail.com>
2022-10-10 16:42:07 +0200

    helper module for reading data

---
+++ src/common/file_management.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+TRAIN_FILEPATH, TEST_FILEPATH = "../data/train.csv", "../data/test.csv"
+
+
+def get_train_higgs_data():
+    """
+    Returns:
+    """
+    with open(TRAIN_FILEPATH, 'r') as file_handle:
+        data = np.genfromtxt(file_handle, delimiter=',', skip_header=1, dtype=object)
+
+        # extract id and predictions from data
+        ids = data[:, :1].astype(int)
+        predictions = data[:, 1:2].astype(str)
+        # encode class predictions to strings
+        predictions = np.unique(predictions, return_inverse=True)[1]
+
+        # skip ID and prediction
+        data = data[:, 2:].astype(float)
+        file_handle.close()
+
+    with open(TRAIN_FILEPATH, 'r') as file_handle:
+        features = np.genfromtxt(file_handle, delimiter=',', max_rows=1, dtype=str)
+        file_handle.close()
+
+    return ids, predictions, data, features
+
+
+def get_test_higgs_data():
+    """
+    Returns:
+    """
+    with open(TEST_FILEPATH, 'r') as file_handle:
+        data = np.genfromtxt(file_handle, delimiter=',', skip_header=1, dtype=object)
+
+        # extract id and predictions from data
+        ids = data[:, :1].astype(int)
+        predictions = data[:, 1:2].astype(str)
+        # encode class predictions to strings
+        predictions = np.unique(predictions, return_inverse=True)[1]
+
+        # skip ID and prediction
+        data = data[:, 2:].astype(float)
+        file_handle.close()
+
+    with open(TEST_FILEPATH, 'r') as file_handle:
+        features = np.genfromtxt(file_handle, delimiter=',', max_rows=1, dtype=str)
+        file_handle.close()
+
+    return ids, predictions, data, features
