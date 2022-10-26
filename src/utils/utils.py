f3091a142691cee1bb58bf036ebf7bb455d8ec2a
bedkowski.patrick <bedkowskisite@gmail.com>
2022-10-26 13:32:34 +0200

    Added least_squares implementation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -59,3 +59,21 @@ def get_confidence_interval(arr, sensitivity):
     lower_limit = q25 - sensitivity*iqr
     upper_limit = q75 + sensitivity*iqr
     return lower_limit, upper_limit
+
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
