1d34c1834cb12ac610fb3732b51d083996d6b6a4
IsaacBattles <49080381+IsaacBattles@users.noreply.github.com>
2022-10-31 14:23:13 +0100

    Updated implementations

--- src/implementations.py
+++ src/implementations.py
@@ -245,38 +245,23 @@ def sigmoid(x):
 
 
 def ridge_regression(y, tx, lambda_):
+    """Perform Ridge Regression
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N, D)
+        lambda_: Scalar
+
+    Returns:
+        loss: scalar number
+        w: shape=(D, 1)
+    """
     LAMBDA = 2 * len(y) * lambda_
 
     # Solve Normal Equations with Regularization Term
     w = np.linalg.solve((tx.T @ tx + (np.eye(len(tx[1])) * LAMBDA)), tx.T @ y)
+    
+    # Compute loss function
+    loss = compute_loss(y, tx, w, type='mse')
 
-    return w
-
-
-# Compute the gradient
-def compute_gradient(y, tx, w):
-    err = y - tx.dot(w)
-    grad = - tx.T.dot(err) / len(err)
-
-    return grad
-
-
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-
-    for n_iter in range(max_iters):
-        grad = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w, type='mse')
-
-        w = w - gamma * grad
-
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
-    return losses, ws
+    return w, loss
